708bd3b57535cde9e9ced88c2f76481c9096cb97
rodgersochieng <sirrodgers21@gmail.com>
2022-11-26 05:57:05 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validates :title, exclusion: ["True Facts"]
+
+
+
 end
